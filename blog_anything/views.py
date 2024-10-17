9ec10ec17808e0aba1190141fa4068b17e764dd4
TeD <theooye7@gmail.com>
2024-10-17 14:53:12 +0100

    html error corrected

--- blog_anything/views.py
+++ blog_anything/views.py
@@ -4,9 +4,9 @@ from .models import Section, Post
 # Create your views here.
 def section_list(request):
     sections = Section.objects.all()
-    return render(request, 'blog/section_list.html', {'sections': sections})
+    return render(request, 'blog_anything/section_list.html', {'sections': sections})
 
 def post_list(request, section_slug):
     section = get_object_or_404(Section, slug=section_slug)
     posts = section.posts.all()
-    return render(request, 'blog/post_list.html', {'section': section, 'posts': posts})
+    return render(request, 'blog_anything/post_list.html', {'section': section, 'posts': posts})
