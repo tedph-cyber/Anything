825e8d29dbc7db8de63f626549968b0385da1ac3
Tedph <theooye7@gmail.com>
2024-11-13 20:17:24 +0100

    again?:(

--- blog_anything/urls.py
+++ blog_anything/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.section_list, name='section_list'),
-    path('<slug:section_slug>/', views.post_list, name='post_list'),
+    path("", views.home, name="home"),
+    path("<slug:section_slug>/", views.post_list, name="post_list"),
+    path("login/", views.login_view, name="login"),
+    path("register/", views.register_view, name="register"),
 ]
--- blog_anything/views.py
+++ blog_anything/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect,  get_object_or_404
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm
 from .models import Section, Post
 
+
 # Create your views here.
+def home(request):
+    return render(request, "blog_anything/index.html")
+
+
 def section_list(request):
     sections = Section.objects.all()
     return render(request, 'blog_anything/section_list.html', {'sections': sections})
@@ -10,3 +17,23 @@ def post_list(request, section_slug):
     section = get_object_or_404(Section, slug=section_slug)
     posts = section.posts.all()
     return render(request, 'blog_anything/post_list.html', {'section': section, 'posts': posts})
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+    return render(request, "blog/login.html")
+
+
+def register_view(request):
+    form = UserCreationForm(request.POST or None)
+    if form.is_valid():
+        user = form.save()
+        login(request, user)
+        return redirect("home")
+    return render(request, "blog/register.html", {"form": form})
